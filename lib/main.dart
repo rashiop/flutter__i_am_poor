5de6a987d0a85f70ffd4e19b2052305b59369682
Poppy Sari <poppy.sari@rga.com>
2021-10-24 22:07:41 +0700

    :rocket: : bump

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: (Scaffold(
+          backgroundColor: Colors.blueGrey[500],
+          appBar: AppBar(
+            backgroundColor: Colors.grey[500],
+            title: Text('I am Poor'),
+          ),
+          body: Center(child: Image(image: AssetImage('images/coal.png')))))));
+}

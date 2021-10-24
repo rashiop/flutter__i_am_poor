e958afbb61c75fe923d29a874bfe57f3ac2b01ab
Poppy Sari <poppy.sari@rga.com>
2021-10-24 22:12:55 +0700

    css: play with title

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,16 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       home: (Scaffold(
-          backgroundColor: Colors.blueGrey[500],
-          appBar: AppBar(
-            backgroundColor: Colors.grey[500],
-            title: Text('I am Poor'),
-          ),
-          body: Center(child: Image(image: AssetImage('images/coal.png')))))));
+    backgroundColor: Colors.blueGrey[500],
+    appBar: AppBar(
+      backgroundColor: Colors.grey[500],
+      title: Text('I am Poor', style: TextStyle(color: Colors.red[300])),
+    ),
+    body: Center(
+      child: Image(
+        image: AssetImage('images/coal.png'),
+        width: 500,
+      ),
+    ),
+  ))));
 }
